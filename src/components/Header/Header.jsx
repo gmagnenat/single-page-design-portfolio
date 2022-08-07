8335e1b4e2a4d7ed314a5a3b33bd679dbefe6dc1
Abby Nyakara <abbynyakara@gmail.com>
2022-08-07 16:36:23 +0300

    add header and intro

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+import './Header.scss'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header__logo">
+        <img src={logo} alt="" />
+      </div>
+      <div className="header__text">
+        <button>Free Consultation</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Intro.scss'
+
+const Intro = () => {
+  return (
+    <div className='intro'>
+      <div className="intro__header">
+        <p>Design solutions made easy</p>
+      </div>
+      <div className="intro__text">
+        <p>With over ten years of experience in various design disciplines,
+         I’m your one-stop shop for your design needs.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Intro
