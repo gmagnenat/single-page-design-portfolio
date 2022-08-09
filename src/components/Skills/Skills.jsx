c8ae2973987482046349ddd4533fb48c0e74d38c
Gwenaël Magnenat <g.magnenat@gmail.com>
2022-08-09 18:01:29 -0400

    Add grid system for the skillCards, add styling for the skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,17 +1,52 @@
 import React from 'react';
-import './Skills.scss';
 import SkillCard from '../SkillCard/SkillCard';
-import img1 from '../../assets/pattern-graphic-design.svg';
-import img2 from '../../assets/pattern-illustrations.svg';
-import img3 from '../../assets/pattern-motion-graphics.svg';
-import img4 from '../../assets/pattern-photography.svg';
-import img5 from '../../assets/pattern-ui-ux.svg';
-import img6 from '../../assets/pattern-apps.svg';
+import imgGraphicDesign from '../../assets/pattern-graphic-design.svg';
+import imgIllustrations from '../../assets/pattern-illustrations.svg';
+import imgMotionGraphics from '../../assets/pattern-motion-graphics.svg';
+import imgPhotography from '../../assets/pattern-photography.svg';
+import imgUiUx from '../../assets/pattern-ui-ux.svg';
+import imgApps from '../../assets/pattern-apps.svg';
+import './Skills.scss';
 
 const Skills = () => {
     return (
         <div className="skills_grid">
-            <SkillCard image={img1} skill={'Graphic Design'} bgColor="blue" />
+            <SkillCard
+                image={imgGraphicDesign}
+                skill={'Graphic Design'}
+                bgColor="blue"
+                className="col-12"
+            />
+            <SkillCard
+                image={imgUiUx}
+                skill={'UI/UX'}
+                bgColor="yellow"
+                className="col-6"
+            />
+            <SkillCard
+                image={imgApps}
+                skill={'Apps'}
+                bgColor="pink"
+                className="col-6"
+            />
+            <SkillCard
+                image={imgIllustrations}
+                skill={'Illustrations'}
+                bgColor="orange"
+                className="col-12"
+            />
+            <SkillCard
+                image={imgPhotography}
+                skill={'Photography'}
+                bgColor="cyan"
+                className="col-12"
+            />
+            <SkillCard
+                image={imgMotionGraphics}
+                skill={'Motion Graphics'}
+                bgColor="purple"
+                className="col-12"
+            />
         </div>
     );
 };
