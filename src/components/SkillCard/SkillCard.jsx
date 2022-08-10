e12a83c9fb4906ffaad7b3da03aafee2c35d7fac
Gwenaël Magnenat <g.magnenat@gmail.com>
2022-08-09 20:25:20 -0400

    Fixing repo

--- src/components/SkillCard/SkillCard.jsx
+++ src/components/SkillCard/SkillCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './SkillCard.scss';
 
-const SkillCard = ({ image, skill, bgColor }) => {
+const SkillCard = ({ image, skill, bgColor, className }) => {
     return (
-        <div className={`skillCard bg-${bgColor}`}>
+        <div className={`skillCard bg-${bgColor} ${className}`}>
             <img className="skillCard__image" src={image} alt="" />
             <p className="skillCard__text">{skill}</p>
         </div>
